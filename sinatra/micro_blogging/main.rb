5c0f11cbd7660ca2ae5cbf8a1eb33ecfffb37175
nauticalnova <nauticalnova@hotmail.com>
2017-02-02 17:31:01 -0700

    finish the backend except for search user by email

--- sinatra/micro_blogging/main.rb
+++ sinatra/micro_blogging/main.rb
@@ -16,7 +16,6 @@ def current_user
 		@user = User.find(session[:user_id])
 	end
 end
-
 get '/' do 
 	current_user
 	if session[:user_id]
@@ -25,7 +24,6 @@ get '/' do
 		erb :home
 	end
 end
-
 post '/update' do 
 	current_user
 
@@ -36,8 +34,6 @@ post '/update' do
 		erb :edit_user
 	end
 end
-
-
 post '/old_user' do 
 	current_user
 	if User.where(email: params[:user][:email]).count != 0
@@ -55,13 +51,11 @@ post '/old_user' do
 		redirect '/login'
 	end
 end 
-
 get '/log_out' do
 	current_user 
 	session[:user_id] = nil
 	redirect '/'
 end
-
 post '/new_user' do 
 	current_user
 	if User.where(email: params[:user][:email]).count == 0
@@ -80,47 +74,38 @@ post '/new_user' do
 
 	end
 end 
-
 get '/userpage' do 
 	current_user
 	@post = @user.posts.all
 	erb :userpage
 end
-
 get '/login' do 
 	current_user
 
 
 	erb :login
 end
-
-post '/edit_user' do 
+get '/edit_user' do 
 	current_user
 
 	erb :edit_user
-
 end 
-
 get '/blog_post' do 
 	current_user
 
 	erb :blogpost 
 end
-
 get '/sign_up' do 
 	current_user
 
 	erb :sign_up
-
 end
-
-delete '/delete_user' do 
+get '/delete_user' do 
 	current_user
 	@user.destroy
 	session[:user_id] = nil
 	redirect '/'
 end
-
 post '/create_post' do 
 	current_user
 	if params[:post][:title] != '' && params[:post][:content] != '' 
@@ -138,14 +123,12 @@ post '/create_post' do
 		redirect '/blog_post'
 	end
 end 
-
 get '/feed' do 
 	current_user
 	@posts = Post.all.order(date_created: :desc).limit(10)
 	@users = User.all
 	erb :feed
 end
-
 get '/create_comment' do 
 	current_user
 	if params[:post_id]
@@ -154,9 +137,7 @@ get '/create_comment' do
 	@post = Post.find(current_comment)
 	
 	erb :create_comment
-
 end 
-
 get '/view_comment' do 
 	current_user
 	current_comment = params[:post_id]
@@ -166,9 +147,7 @@ get '/view_comment' do
 	
 	@comments = Comment.where(post_id: current_comment)
 	erb :view_post_comments
-
 end 
-
 post '/make_comment' do 
 	current_user
 	@post = Post.find(current_comment)
@@ -189,9 +168,7 @@ post '/make_comment' do
 		flash[:alert] = 'Please make sure you fill out comment'
 		reroute '/create_comment'
 	end	
-
 end 
-
 get '/edit_post' do 
 	current_user
 	current_post = params[:post_id]
@@ -199,10 +176,7 @@ get '/edit_post' do
 
 
 	erb :edit_posts
-
 end 
-
-
 post '/edit_post' do 
 	@post = Post.find(current_post)
 	if params[:post][:content]
@@ -215,26 +189,20 @@ post '/edit_post' do
 			redirect '/userpage'
 		end
 	end
-
 end 
-
 get '/delete_post' do 
 	current_user
 	@post = Post.find(params[:post_id])
 	@post.destroy
 	redirect '/'
-
 end 
-
 get '/edit_comment' do 
 	current_user
 	current_comment = params[:comment_id]
 	@comment = Comment.find(current_comment)
 
 	erb :edit_comments
-
 end 
-
 post '/edit_comment' do 
 
 	@comment = Comment.find(current_comment)
@@ -248,19 +216,13 @@ post '/edit_comment' do
 			redirect '/userpage'
 		end
 	end
-
-
 end 
-
-
 get '/delete_comment' do 
 	current_user
 	@comment = Comment.find(params[:comment_id])
 	@comment.destroy
 	redirect '/'
-
 end 
-
 get '/user/:id' do 
 	current_user
 	@viewed_user = User.find(params[:id])
@@ -268,7 +230,6 @@ get '/user/:id' do
 
 
 	erb :view_user
-
 end 
 
 
